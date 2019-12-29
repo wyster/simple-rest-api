54994ec5b07d59ebae6fe3ee82e12b9789df70a9
Ilya Zelenin <wyster@make.im>
2019-12-30 00:10:22 +0300

    chore: xdebug disabled by default

--- .docker/php/start.sh
+++ .docker/php/start.sh
@@ -5,6 +5,10 @@ if [ $COMPOSER_INSTALL == "1" ]; then
     composer install --prefer-dist --no-progress --no-suggest
 fi
 
+if [ $ENABLE_XDEBUG == "1" ]; then
+    docker-php-ext-enable xdebug
+fi
+
 ./vendor/bin/phinx migrate
 
 docker-php-entrypoint php-fpm
