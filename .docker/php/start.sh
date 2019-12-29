43599b77da97f797f36193c91827d764b7fcbaca
Ilya Zelenin <wyster@make.im>
2019-12-29 14:33:09 +0300

    chore: exclude from analyse

--- .docker/php/start.sh
+++ .docker/php/start.sh
@@ -2,7 +2,7 @@
 
 if [ $COMPOSER_INSTALL == "1" ]; then
     composer global require hirak/prestissimo
-    composer install
+    composer install --prefer-dist --no-progress --no-suggest
 fi
 
 ./vendor/bin/phinx migrate
